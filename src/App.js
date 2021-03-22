47bd6e30c9b963d7ab3a01bc819bf1fbf498920c
eyouale <teyouale@gmail.com>
2021-03-22 17:38:50 +0300

    Creating Input Form and Targeting Event Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const changeValue = (e) => {
+    setInputValue([e.target.value]);
+  };
   return (
-    <div className="App">
-      Removing Unwanted Files and Adding Bootstrap Dependency
-    </div>
+    <Container>
+      <Form>
+        <Form.Group>
+          {/*<Form.Label>Email User Name</Form.Label>*/}
+          <Form.Control
+            size="lg"
+            type="text"
+            placeholder="Enter Github User Name"
+            onChange={changeValue}
+            value={inputValue}
+          />
+        </Form.Group>
+      </Form>
+      {console.log(inputValue)}
+    </Container>
   );
 }
 
